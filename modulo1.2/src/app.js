cafb75ffd30dc74d05b959240a91662fe8006506
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2023-04-03 20:49:51 -0300

    composicao

--- modulo1.2/src/app.js
+++ modulo1.2/src/app.js
@@ -1,19 +1,16 @@
 'use strict';
 
 import React, { Component } from 'react';
-import Scare from './scare';
+import Button from './button';
+import LikeButton from './like-button';
+import SearchButton from './search-button';
 
 class App extends Component {
   render() {
     return (
-      <div
-        className="container"
-        data-id="1"
-        onClick={(e) => {
-          alert('clicou');
-        }}
-      >
-        <Scare />
+      <div className="container">
+        <LikeButton />
+        <SearchButton />
       </div>
     );
   }
