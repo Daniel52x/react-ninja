e3a62c5dc913859151aa3f8eb5b69247733fb234
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2023-04-03 22:27:10 -0300

    state

--- modulo1.2/src/app.js
+++ modulo1.2/src/app.js
@@ -1,26 +1,26 @@
 'use strict';
 
 import React, { Component } from 'react';
+import Button from './button';
+import Square from './scare';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      text: 'Fernando',
+      color: 'green',
     };
   }
 
   render() {
     return (
-      <div
-        className="container"
-        onClick={() =>
-          this.setState({
-            text: 'Outro texto',
-          })
-        }
-      >
-        {this.state.text}
+      <div className="container">
+        <Square color={this.state.color} />
+        {['red', 'green', 'blue'].map((color) => (
+          <Button key={color} handleClick={() => this.setState({ color })}>
+            {color}
+          </Button>
+        ))}
       </div>
     );
   }
