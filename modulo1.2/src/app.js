44c9e0b7a63f58d62e9425918d418c915578436b
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2023-04-10 20:18:12 -0300

    montagem / desmontagem

--- modulo1.2/src/app.js
+++ modulo1.2/src/app.js
@@ -1,26 +1,37 @@
 'use strict';
 
 import React, { Component } from 'react';
-import Button from './button';
-import Square from './scare';
+import Timer from './timer';
 
 class App extends Component {
   constructor() {
+    console.log('constructor');
     super();
     this.state = {
-      color: 'green',
+      showTimer: true,
     };
   }
 
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentWillUnmount() {}
+
   render() {
+    console.log('render');
     return (
-      <div className="container">
-        <Square color={this.state.color} />
-        {['red', 'green', 'blue'].map((color) => (
-          <Button key={color} handleClick={() => this.setState({ color })}>
-            {color}
-          </Button>
-        ))}
+      <div>
+        {this.state.showTimer && <Timer />}
+        <button
+          onClick={() => this.setState({ showTimer: !this.state.showTimer })}
+        >
+          Show / hide timer
+        </button>
       </div>
     );
   }
